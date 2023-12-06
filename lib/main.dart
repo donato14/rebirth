5af79ab8a7d61b24dfb75ddcc666c8c3df992952
donato14 <doradoraaa132@gmail.com>
2023-12-07 00:26:54 +0900

    기본 레이아웃 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const myApp());
+  runApp(const MyApp());
 }
 
-class myApp extends StatelessWidget {
-  const myApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +32,59 @@ class myApp extends StatelessWidget {
             style: TextStyle(color: Colors.black87),
           ),
           backgroundColor: Colors.white,
+        ),
+        body: const SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 35),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '오늘 하루,',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          '새롭게 보호된 유기동물',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              '163',
+                              style: TextStyle(
+                                fontSize: 28,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                            Text(
+                              ' 마리',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Text('(대충 동물 픽토그램)'),
+                  ],
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  '입양은 어떠세요?',
+                  style: TextStyle(fontSize: 18),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
